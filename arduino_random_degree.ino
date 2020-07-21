d042b432c6bc45449118f5ede61878bbad76371c
Oskar <case112@users.noreply.github.com>
2020-07-21 20:07:50 +0300

    Create arduino_random_degree.ino

---
+++ arduino_random_degree.ino
@@ -0,0 +1,33 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display(-1);
+
+void setup()   
+{                
+  // initialize with the I2C addr 0x3C
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+
+}
+
+void loop() {
+  
+  // Display Text
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(5,7);
+  display.print((char)247); // degree symbol 
+  display.print("C ");
+  display.println(random(9, 199)); 
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  delay(3000);
+  
+  }
